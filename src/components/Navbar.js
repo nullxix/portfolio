349d033d5c00fb19beb7839a3068b4b887e8ab4f
Nicholas Black <57681918+nullxix@users.noreply.github.com>
2020-02-27 19:29:51 -0700

    installed brunch

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Title from './Title.js'
+import NavItem from './NavItem.js'
+import {Link} from 'react-router-dom'
+export default props => {
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                padding: '10px'
+            }}
+        >
+            <Link to="/"><Title/></Link>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    width: '70%',
+                    paddingTop: '24px'
+                }}
+            >
+                <Link to='/projects'><NavItem>Projects</NavItem></Link>
+                {/* <Link to='/websites'><NavItem>Websites</NavItem></Link> */}
+                <Link to='/hire'><NavItem>Hire</NavItem></Link>
+                {/* <Link to='/else'><NavItem>Everything Else</NavItem></Link> */}
+            </div>
+        </div>
+    )
+}
---
+++ src/svg/rhombus-22.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Icon(props) {
+  return (
+    <>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="526"
+      height="182.79"
+      viewBox="54.625 199 530 186.79"
+    >
+      <defs>
+        <path id="a" d="M55.63 200l526 21.9v138.99l-526 21.9V200z">
+        </path>
+      </defs>
+      <use fill="#5b15b8" fillOpacity="0" xlinkHref="#a"></use>
+      <use fillOpacity="0" stroke="#e9861c" xlinkHref="#a"></use>
+    </svg>
+    </>
+  );
+}
+
+export default Icon;
